08430dbd7e67838f5ab77a47007758b4d7ec9538
valentin <valyo.mir@gmail.com>
2017-02-14 09:28:16 +0000

    changed post methods

--- routes/user/user.js
+++ routes/user/user.js
@@ -5,14 +5,26 @@ var firebase = require('firebase');
 router.get('/logout', function (req, res) {
     firebase.auth().signOut().then(function () {
             res.redirect('/');
+            res.locals.session = null;
+            req.app.locals.babysitter = false;
+            req.app.locals.client = false;
+            req.app.locals.user = null;
+            express.session = null;
         }
     ).catch(function (error) {
         res.send(error);
     })
 });
 
-router.get('/', function (req, res) {
+router.get('/', isLoggedIn, function (req, res) {
     res.render('user/profile',{name:"test"});
 });
+
+function isLoggedIn(req,res,next) {
+    if(req.app.locals.user){
+        return next();
+    }
+    res.redirect('/login');
+}
 
 module.exports = router;
