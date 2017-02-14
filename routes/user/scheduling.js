0430bd47059b13b2a6ff5fffef39ae04ad17ddd2
Nuno <ludovice.nuno@gmail.com>
2017-02-14 12:09:49 +0000

    schedule view form add

--- routes/user/scheduling.js
+++ routes/user/scheduling.js
@@ -2,11 +2,22 @@ var express = require('express');
 var router = express.Router();
 var firebase = require('firebase');
 
+
 router.get('/', function (req, res) {
     res.render('user/scheduling');
 });
 
-router.post('/schedule', function (req, res) {
+router.post('/scheduling', function (req, res) {
+    var dayMonth = req.body.dayMonth.toString();
+    var startHour = req.body.startHour.valueOf().toString();
+    var duration = req.body.duration;
+    req.body.result.innerHTML(dayMonth);
+    console.log(dayMonth);
+    console.log(startHour);
+    console.log(duration);
+
+    //var scheduleQuery = "SELECT * from schedule where day_month = 'dayMonth' and
+    // start_hour = 'start_hour' and duration = 'duration'  ";
 
 });
 module.exports = router;
