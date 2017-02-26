17ab91493bf068b2e480128e99463975d0599541
Valentin <valyo.mir@gmail.com>
2017-02-26 17:30:58 +0000

    fixed history view

--- routes/user/schedules.js
+++ routes/user/schedules.js
@@ -3,7 +3,9 @@ var router = express.Router();
 var firebase = require('firebase');
 var util = require('util');
 
-var future_visits = "SELECT * FROM visits WHERE date >= NOW() and id_babysitter = %s";
+var future_visits = "SELECT b.name as babysitter_name, c.name as client_name, (v.duration*b.price) as price," +
+" v.date, v.start_hour, v.duration, v.evaluation, c.address FROM visits v inner join clients c on v.id_client = c.id" +
+" inner join babysitter b on b.id = v.id_babysitter WHERE date >= NOW() and id_babysitter = %s";
 
 const connection = require('../../models/Connect');
 
