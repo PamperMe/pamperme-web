79c4487932e4cf347ffa9310d8fb94feb0d77e57
Valentin <valyo.mir@gmail.com>
2017-02-27 19:22:36 +0000

    change header at 768 pixels

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -1,6 +1,13 @@
 $(function () {
   $('[data-toggle="tooltip"]').tooltip()
 });
+$(window).resize(function(){
+    if($(window).width()<=768){
+        $('.menu-nav').css({display:"none"});
+    }else{
+        $('.menu-nav').css({display:"block"});
+    }
+});
 $('.btn-sso').click(function(){
     if($('.menu-nav').css("display") != "inline"){
         $('.menu-nav').css({display:"inline"});
